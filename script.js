3845eb08f79d9e93a133b6371f1f064d82340da5
saadkamal219 <saadkamal219@gmail.com>
2024-11-19 02:16:34 +0600

    Content

---
+++ script.js
@@ -0,0 +1,5 @@
+//navigation bar effects
+window.addEventListener("scroll", function(){
+    const header = this.document.querySelector("header");
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+});
