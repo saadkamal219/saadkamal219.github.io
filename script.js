ed76a905834aafe5ddd9ce3ffe5cb9f60346f841
saadkamal219 <saadkamal219@gmail.com>
2024-11-20 12:44:30 +0600

    Content

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ window.addEventListener("scroll", function(){
     const header = this.document.querySelector("header");
     header.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+//project model pop up
+const projectModels = document.querySelectorAll(".project-model");
+const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    projectModels[modalClick].classList.add("active");
+}
+
+learnMoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        projectModels.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
